cdbbe846a3a6c138f3d8d798fd1d9c706d1d5b36
Matadysa <matadysa@gmail.com>
2022-06-09 12:22:40 +0430

    static methods added to user

--- src/Model/User.java
+++ src/Model/User.java
@@ -1,14 +1,22 @@
 package Model;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.UUID;
 
 public class User {
+    private static HashMap<String, User> users = new HashMap<>();
     private final String id;
     private int balance = 0;
     private Cart cart = null;
 
+    public static User getUserById(String id) {
+        return users.get(id);
+    }
+
     public User() {
         this.id = UUID.randomUUID().toString();
+        users.put(this.id, this);
     }
 
     public String getId() {
